1f1bd5912a18422f6349d4b4607d87bdac7cbde1
Leena Bhegade <bhegde.leena@gmail.com>
2019-07-24 20:12:11 +0100

    fix identation issues

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,9 +1,9 @@
 class Pizza < ApplicationRecord
-	validates :name, presence: true
-	serialize :ingredients
+  validates :name, presence: true
+  serialize :ingredients
 
-	def serializable_hash(options={})
-	  options[:except] ||= [:created_at, :updated_at]
-	  super(options)
-	end
+  def serializable_hash(options={})
+    options[:except] ||= [:created_at, :updated_at]
+    super(options)
+  end
 end
