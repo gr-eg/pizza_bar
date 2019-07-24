b126a75385d970a28f82759e19323b7ff01ad319
Leena Bhegade <bhegde.leena@gmail.com>
2019-07-24 20:14:27 +0100

    fix identation issue in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def not_found
-	  render plain: "404 Not Found", status: 404
+  	render plain: "404 Not Found", status: 404
   end
 end
